fbf9009285fd293be609c988de5e28c96c0f3588
pengdd <pengdingding@lumlord.com>
2020-11-20 09:07:59 +0800

    update: 开启了swagger文档

--- src/main/java/com/pdd/SpringbootApplication.java
+++ src/main/java/com/pdd/SpringbootApplication.java
@@ -2,8 +2,10 @@ package com.pdd;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = DataSourceAutoConfiguration.class)
+
 public class SpringbootApplication {
 
     public static void main(String[] args) {
