1a847b202c98da98ce7308288cd24c17e44f6dd7
pengdd <pengdingding@lumlord.com>
2021-01-04 15:09:14 +0800

    add: 日志

---
+++ src/main/java/com/pdd/controller/ranking/RankingController.java
@@ -0,0 +1,44 @@
+package com.pdd.controller.ranking;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @author pdd
+ * @email pengdingding@lumlord.com
+ * @date 2021/1/4  14:13:33
+ * @description 排行榜
+ * ////////////////////////////////////////////////////////////////////
+ * //                          _ooOoo_                               //
+ * //                         o8888888o                              //
+ * //                         88" . "88                              //
+ * //                         (| ^_^ |)                              //
+ * //                         O\  =  /O                              //
+ * //                      ____/`---'\____                           //
+ * //                    .'  \\|     |//  `.                         //
+ * //                   /  \\|||  :  |||//  \                        //
+ * //                  /  _||||| -:- |||||-  \                       //
+ * //                  |   | \\\  -  /// |   |                       //
+ * //                  | \_|  ''\---/''  |   |                       //
+ * //                  \  .-\__  `-`  ___/-. /                       //
+ * //                ___`. .'  /--.--\  `. . ___                     //
+ * //              ."" '<  `.___\_<|>_/___.'  >'"".                  //
+ * //            | | :  `- \`.;`\ _ /`;.`/ - ` : | |                 //
+ * //            \  \ `-.   \_ __\ /__ _/   .-` /  /                 //
+ * //      ========`-.____`-.___\_____/___.-`____.-'========         //
+ * //                           `=---='                              //
+ * //      ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^        //
+ * //            佛祖保佑       永不宕机     永无BUG                  //
+ * ////////////////////////////////////////////////////////////////////
+ */
+@RestController
+@RequestMapping("/ranking")
+public class RankingController {
+
+    private static final Logger logger = LoggerFactory.getLogger(RankingController.class);
+
+
+
+}
